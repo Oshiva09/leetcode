f0fb9d29eab648cea0bbd760b5be13ed8127fd72
Oshiva Singh <oshiva.singh_cs.aiml24@gla.ac.in>
2026-09-19 22:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (38.51%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -1,6 +1,33 @@
 class Solution {
     public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+        int n = nums.length;
+        int l = 0;
+        int h = n - 1;
+        int ans = Integer.MAX_VALUE;
+
+        if (n == 1)
+            return nums[0];
+
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+
+            // Duplicate case
+            if (nums[l] == nums[m] && nums[m] == nums[h]) {
+                ans = Math.min(ans, nums[l]);  // CHANGE
+                l++;
+                h--;
+                continue;
+            }
+
+            if (nums[l] <= nums[m]) {
+                ans = Math.min(ans, nums[l]);
+                l = m + 1;
+            } else {
+                ans = Math.min(ans, nums[m]);
+                h = m - 1;
+            }
+        }
+
+        return ans;
     }
 }
